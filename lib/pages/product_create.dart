073234add2170598844b72ec7ae3388a386e6c1d
Azamat <azamat87@gmail.com>
2020-04-04 21:46:05 +0600

    MediaQuery

--- lib/pages/product_create.dart
+++ lib/pages/product_create.dart
@@ -64,16 +64,20 @@ class _ProductCreatePageState extends State<ProductCreatePage> {
 
   @override
   Widget build(BuildContext context) {
+    final double deviceWidth = MediaQuery.of(context).size.width;
+    final double targetWidth = deviceWidth > 550.0 ? 500.0 : deviceWidth * 0.95;
+    final double targetPadding = deviceWidth - targetWidth;
+
     return Container(
       margin: EdgeInsets.all(10.0),
       child: ListView(
+        padding: EdgeInsets.symmetric(horizontal: targetPadding / 2),
         children: <Widget>[
           _buildTitleTextField(),
           _buildDescriptionTextField(),
           _buildPriceTextField(),
           SizedBox(height: 10.0),
           RaisedButton(
-            color: Theme.of(context).accentColor,
             textColor: Colors.white,
             child: Text('Save'),
               onPressed: _submitForm
