003ff5773685e77905fc304e0d7a098dd114fe42
Azamat <azamat87@gmail.com>
2020-04-05 12:52:47 +0600

    Dismissible

--- lib/pages/product_list.dart
+++ lib/pages/product_list.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutterapp/constants.dart';
 import 'package:flutterapp/pages/product_edit.dart';
 
-
 class ProductListPage extends StatelessWidget {
   final List<Map<String, dynamic>> products;
   final Function updateProduct;
@@ -11,26 +10,39 @@ class ProductListPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(itemBuilder: (BuildContext context, int index) {
-      return ListTile(
-        leading: Image.asset(products[index][IMAGE]),
-        title: Text(products[index][TITLE]),
-        trailing: IconButton(icon: Icon(Icons.edit),
-          onPressed: () {
-            Navigator.of(context).push(
-              MaterialPageRoute(
-                builder: (BuildContext context) {
-                  return ProductEditPage(
-                    product: products[index],
-                    updateProduct: updateProduct,
-                    productIndex: index,);
-                }
-              )
-            );
-          },
-        ),
-      );
-    },
-    itemCount: products.length,);
+    return ListView.builder(
+      itemBuilder: (BuildContext context, int index) {
+        return Dismissible(
+          key: Key(products[index][TITLE]),
+          background: Container(color: Colors.red,),
+          child: Column(
+            children: <Widget>[
+              ListTile(
+                leading: CircleAvatar(
+                    backgroundImage: AssetImage(products[index][IMAGE],
+                )),
+                title: Text(products[index][TITLE]),
+                subtitle: Text('\$${products[index][PRICE].toString()}'),
+                trailing: IconButton(
+                  icon: Icon(Icons.edit),
+                  onPressed: () {
+                    Navigator.of(context)
+                        .push(MaterialPageRoute(builder: (BuildContext context) {
+                      return ProductEditPage(
+                        product: products[index],
+                        updateProduct: updateProduct,
+                        productIndex: index,
+                      );
+                    }));
+                  },
+                ),
+              ),
+              Divider()
+            ],
+          ),
+        );
+      },
+      itemCount: products.length,
+    );
   }
 }
