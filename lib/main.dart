48a0e50255242f88c210703f5db87307ca5b882d
Azamat <azamat87@gmail.com>
2020-04-08 19:44:24 +0600

    Provide Scoped Model

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import 'package:flutterapp/pages/auth.dart';
 import 'package:flutterapp/pages/product.dart';
 import 'package:flutterapp/pages/products.dart';
 import 'package:flutterapp/pages/products_admin.dart';
+import 'package:flutterapp/scoped_model/products.dart';
+import 'package:scoped_model/scoped_model.dart';
 
 void main() {
   // debugPaintSizeEnabled = true;
@@ -25,38 +27,41 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      // debugShowMaterialGrid: true,
-      theme: ThemeData(
-          brightness: Brightness.light,
-          primarySwatch: Colors.deepOrange,
-          accentColor: Colors.deepPurple,
-          buttonColor: Colors.deepPurple),
+    return ScopedModel<ProductModel>(
+      model: ProductModel(),
+      child: MaterialApp(
+        // debugShowMaterialGrid: true,
+        theme: ThemeData(
+            brightness: Brightness.light,
+            primarySwatch: Colors.deepOrange,
+            accentColor: Colors.deepPurple,
+            buttonColor: Colors.deepPurple),
 //      home: AuthPage(),
-      routes: {
-        '/': (BuildContext context) => AuthPage(),
-        '/products': (BuildContext context) => ProductsPage(),
-        '/admin': (BuildContext context) => ProductAdminPage(),
-      },
-      onGenerateRoute: (RouteSettings settings) {
-        final List<String> pathElements = settings.name.split('/');
-        print(pathElements);
-        if(pathElements[0] != '') {
-          return null;
-        }
+        routes: {
+          '/': (BuildContext context) => AuthPage(),
+          '/products': (BuildContext context) => ProductsPage(),
+          '/admin': (BuildContext context) => ProductAdminPage(),
+        },
+        onGenerateRoute: (RouteSettings settings) {
+          final List<String> pathElements = settings.name.split('/');
+          print(pathElements);
+          if(pathElements[0] != '') {
+            return null;
+          }
 
-        if(pathElements[1] == 'product'){
-          final int index = int.parse(pathElements[2]);
-          return MaterialPageRoute<bool>(
-            builder: (BuildContext context) => ProductPage(null, null,null, null),
+          if(pathElements[1] == 'product'){
+            final int index = int.parse(pathElements[2]);
+            return MaterialPageRoute<bool>(
+              builder: (BuildContext context) => ProductPage(null, null,null, null),
+            );
+          }
+          return null;
+        },
+        onUnknownRoute: (RouteSettings settings) {
+          return MaterialPageRoute(builder: (BuildContext context) => ProductsPage()
           );
-        }
-        return null;
-      },
-      onUnknownRoute: (RouteSettings settings) {
-        return MaterialPageRoute(builder: (BuildContext context) => ProductsPage()
-        );
-      },
+        },
+      ),
     );
   }
 }
